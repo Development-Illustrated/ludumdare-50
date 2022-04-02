b7513f36f9f5e3462b325e36eb2e3a3e83f8e131
Luke Berry <luke@spill.chat>
2022-04-02 23:29:55 +0100

    👻🎉 Movement + Anims

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,15 +12,25 @@ public class PlayerController : MonoBehaviour
   }
   private Vector2 moveVector;
   private Rigidbody2D rb;
+
+  private Animator animator;
   private Direction lastDirection;
 
-  private SpriteRenderer spriteRenderer;
+  private float prevMaxSpeed;
+  private float currentSpeed;
 
   [Header("Attributes")]
-  [SerializeField] private float movementSpeed = 3f;
+  [SerializeField] private float accelerationSpeed = 3f;
+  [SerializeField] private float maximumSpeed;
   void Awake()
   {
     rb = gameObject.GetComponent<Rigidbody2D>();
+    animator = gameObject.GetComponentInChildren<Animator>();
+  }
+
+  void Start()
+  {
+    animator.SetBool("isIdle", true);
   }
 
   public void HandleMovement(InputAction.CallbackContext context)
@@ -31,7 +41,8 @@ public class PlayerController : MonoBehaviour
   void FixedUpdate()
   {
     FlipSprite();
-    rb.AddForce(new Vector2(moveVector.x * movementSpeed, moveVector.y * movementSpeed), ForceMode2D.Impulse);
+    UpdateAnimator();
+    rb.AddForce(new Vector2(moveVector.x * accelerationSpeed, moveVector.y * accelerationSpeed), ForceMode2D.Impulse);
   }
 
   private void FlipSprite()
@@ -53,6 +64,31 @@ public class PlayerController : MonoBehaviour
     {
       transform.localScale = Vector3.one;
     }
+  }
 
+  private void UpdateAnimator()
+  {
+    float fuck = Mathf.Abs(rb.velocity.x);
+    prevMaxSpeed = fuck;
+
+    if (fuck > 0 && fuck < 5)
+    {
+      animator.SetBool("isIdle", false);
+      animator.SetBool("isAccelerating", true);
+    }
+
+    if (fuck > 5)
+    {
+      animator.SetBool("isAccelerating", false);
+      animator.SetBool("isMaxSpeed", true);
+    }
+
+    if (fuck < 1)
+    {
+      animator.SetBool("isAccelerating", false);
+      // animator.SetBool("isDecelerating", false);
+      animator.SetBool("isMaxSpeed", false);
+      animator.SetBool("isIdle", true);
+    }
   }
 }
