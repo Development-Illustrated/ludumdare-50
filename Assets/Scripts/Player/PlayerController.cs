3f2c7d6e864da89aa53218ba4ac94df08e7aaeae
Luke Berry <luke@spill.chat>
2022-04-02 20:24:42 +0100

    👻 player movement
    tmp

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+  private Vector2 moveVector;
+  private Rigidbody2D rb;
 
-    }
+  [SerializeField] private float movementSpeed = 3f;
+  void Awake()
+  {
+    rb = gameObject.GetComponent<Rigidbody2D>();
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
+  public void HandleMovement(InputAction.CallbackContext context)
+  {
+    moveVector = context.ReadValue<Vector2>();
+  }
 
-    }
+  void FixedUpdate()
+  {
+
+    rb.AddForce(new Vector2(moveVector.x * movementSpeed * Time.fixedDeltaTime, moveVector.y * movementSpeed * Time.fixedDeltaTime), ForceMode2D.Impulse);
+  }
 }
