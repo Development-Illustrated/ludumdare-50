ef9663d91c02f7b3dba9a6ae7f0f10adcfc727f9
Luke Berry <luke@spill.chat>
2022-04-02 21:25:02 +0100

    👻🕹 ghosty movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,9 +5,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+  enum Direction
+  {
+    LEFT,
+    RIGHT
+  }
   private Vector2 moveVector;
   private Rigidbody2D rb;
+  private Direction lastDirection;
 
+  private SpriteRenderer spriteRenderer;
+
+  [Header("Attributes")]
   [SerializeField] private float movementSpeed = 3f;
   void Awake()
   {
@@ -21,7 +30,29 @@ public class PlayerController : MonoBehaviour
 
   void FixedUpdate()
   {
+    FlipSprite();
+    rb.AddForce(new Vector2(moveVector.x * movementSpeed, moveVector.y * movementSpeed), ForceMode2D.Impulse);
+  }
 
-    rb.AddForce(new Vector2(moveVector.x * movementSpeed * Time.fixedDeltaTime, moveVector.y * movementSpeed * Time.fixedDeltaTime), ForceMode2D.Impulse);
+  private void FlipSprite()
+  {
+    if (rb.velocity.x > 0)
+    {
+      lastDirection = Direction.RIGHT;
+    }
+    else if (rb.velocity.x < 0)
+    {
+      lastDirection = Direction.LEFT;
+    }
+
+    if (lastDirection == Direction.RIGHT)
+    {
+      transform.localScale = new Vector3(-1, 1, 0);
+    }
+    else if (lastDirection == Direction.LEFT)
+    {
+      transform.localScale = Vector3.one;
+    }
+
   }
 }
