402c4009e29217da0e0b2e99ebe34275ab30f789
Luke Berry <luke@spill.chat>
2022-04-03 17:52:56 +0100

    Updated tilemap

--- Assets/Tiled/Tilesets/Office Tileset.tsx
+++ Assets/Tiled/Tilesets/Office Tileset.tsx
@@ -26,9 +26,19 @@
    <object id="1" x="16" y="0" width="16" height="32"/>
   </objectgroup>
  </tile>
- <tile id="23">
+ <tile id="22">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="16" height="32"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="16"/>
   </objectgroup>
  </tile>
  <tile id="33">
@@ -48,22 +58,67 @@
  </tile>
  <tile id="37">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="16"/>
+   <object id="1" x="0" y="0" width="32" height="8"/>
   </objectgroup>
  </tile>
  <tile id="38">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="32" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="8"/>
   </objectgroup>
  </tile>
  <tile id="46">
   <objectgroup draworder="index" id="3">
-   <object id="2" x="22" y="0" width="10" height="32"/>
+   <object id="3" x="0" y="0" width="8" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="48">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="0" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24" y="0" width="8" height="32"/>
   </objectgroup>
  </tile>
  <tile id="61">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="22" y="0" width="10" height="32"/>
+   <object id="2" x="0" y="0" width="8" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="75">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24" y="0" width="8" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="77">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="16" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="78">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="16" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="79">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="16" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="80">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="16" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="81">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="16" width="32" height="16"/>
   </objectgroup>
  </tile>
 </tileset>
