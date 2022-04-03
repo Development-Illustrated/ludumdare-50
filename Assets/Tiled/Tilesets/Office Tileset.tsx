a37b258ff8b7a58b3cd7895b6902558e2216d200
Luke Berry <luke@spill.chat>
2022-04-03 16:12:46 +0100

    ahhhhh

--- Assets/Tiled/Tilesets/Office Tileset.tsx
+++ Assets/Tiled/Tilesets/Office Tileset.tsx
@@ -21,6 +21,16 @@
    <object id="1" x="0" y="0" width="32" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="0" width="16" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="33">
   <objectgroup draworder="index" id="2">
    <object id="1" x="24" y="0" width="8" height="32"/>
@@ -29,6 +39,21 @@
  <tile id="35">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="46">
